f2262394110a7765675e7f56b75e127efd7b1bed
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-29 20:33:25 +0600

    Add auth middleware to protect private routes

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/justinas/nosurf"
 	"github.com/raihan2bd/chatgpt-go/config"
+	"github.com/raihan2bd/chatgpt-go/helpers"
 )
 
 var app *config.Application
@@ -29,4 +30,15 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return app.Session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			app.Session.Put(r.Context(), "error", "Log in first!")
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
